ede4176ef4a761ce6fccf84ed88058f9b401a35b
mahmoudhamdyae <mahmoudhamdyae@gmail.com>
2023-11-03 13:21:53 +0200

    Location Permission

--- app/src/main/java/com/mahmoudhamdyae/weatherforecast/MainActivity.kt
+++ app/src/main/java/com/mahmoudhamdyae/weatherforecast/MainActivity.kt
@@ -1,141 +1,20 @@
 package com.mahmoudhamdyae.weatherforecast
 
 import android.Manifest
-import android.annotation.SuppressLint
-import android.content.Context
-import android.content.Intent
 import android.content.pm.PackageManager
-import android.location.LocationManager
 import android.os.Build
 import android.os.Bundle
-import android.os.Looper
-import android.provider.Settings
-import android.util.Log
-import android.widget.Toast
 import androidx.activity.result.contract.ActivityResultContracts
 import androidx.appcompat.app.AppCompatActivity
-import androidx.core.app.ActivityCompat
 import androidx.core.content.ContextCompat
-import androidx.core.location.LocationManagerCompat.isLocationEnabled
 import androidx.navigation.fragment.NavHostFragment
 import androidx.navigation.ui.NavigationUI.setupWithNavController
 import androidx.preference.PreferenceManager
-import com.google.android.gms.location.*
 import com.google.android.material.bottomnavigation.BottomNavigationView
 import dagger.hilt.android.AndroidEntryPoint
 
-
-private const val PERMISSION_ID = 5005
-
 @AndroidEntryPoint
 class MainActivity : AppCompatActivity() {
-
-    private lateinit var mFusedLocationClient: FusedLocationProviderClient
-
-    override fun onCreate(savedInstanceState: Bundle?) {
-        super.onCreate(savedInstanceState)
-        setContentView(R.layout.activity_main)
-
-        askNotificationPermission()
-
-        // Bottom Navigation
-        val navHostFragment = supportFragmentManager.findFragmentById(R.id.nav_host_fragment) as NavHostFragment
-        val bottomView = findViewById<BottomNavigationView>(R.id.bottom_nav)
-        setupWithNavController(bottomView, navHostFragment.navController)
-
-        mFusedLocationClient = LocationServices.getFusedLocationProviderClient(this)
-    }
-
-    private val mLocationCallback = object: LocationCallback() {
-        override fun onLocationResult(locationResult: LocationResult) {
-            super.onLocationResult(locationResult)
-            val mLastLocation = locationResult.lastLocation
-        }
-    }
-
-    private fun checkPermissions() = ActivityCompat.checkSelfPermission(
-        this,
-        Manifest.permission.ACCESS_COARSE_LOCATION
-    ) == PackageManager.PERMISSION_GRANTED ||
-            ActivityCompat.checkSelfPermission(
-                this,
-                Manifest.permission.ACCESS_COARSE_LOCATION
-            ) == PackageManager.PERMISSION_GRANTED
-
-    private fun requestPermissions() {
-        ActivityCompat.requestPermissions(
-            this,
-            arrayOf(
-                Manifest.permission.ACCESS_COARSE_LOCATION,
-                Manifest.permission.ACCESS_FINE_LOCATION,
-            ),
-            PERMISSION_ID
-        )
-    }
-
-//    override fun onRequestPermissionsResult(
-//        requestCode: Int,
-//        permissions: Array<out String>,
-//        grantResults: IntArray
-//    ) {
-//        super.onRequestPermissionsResult(requestCode, permissions, grantResults)
-//        if (requestCode == PERMISSION_ID) {
-//            if (
-//                grantResults[0] == PackageManager.PERMISSION_GRANTED ||
-//                grantResults[1] == PackageManager.PERMISSION_GRANTED
-//            ) {
-////                getLocation()
-//            }
-//        }
-//    }
-
-//    private fun isLocationEnabled(): Boolean {
-//        val locationManager = getSystemService(Context.LOCATION_SERVICE) as LocationManager
-//        return locationManager.isProviderEnabled(LocationManager.GPS_PROVIDER) ||
-//                locationManager.isProviderEnabled(LocationManager.NETWORK_PROVIDER)
-//    }
-
-//    override fun onResume() {
-//        super.onResume()
-////        getLocation()
-//    }
-
-//    private fun getLocation() {
-//        if (checkPermissions()) {
-//            if (isLocationEnabled()) {
-//                requestNetworkLocalData()
-//            } else {
-//                onLocationNotGranted()
-//                val intent = Intent(Settings.ACTION_LOCATION_SOURCE_SETTINGS)
-//                startActivity(intent)
-//            }
-//        } else {
-//            requestPermissions()
-//        }
-//    }
-
-//    @SuppressLint("MissingPermission")
-//    private fun requestNetworkLocalData() {
-//        val mLocationRequest = LocationRequest.Builder(Priority.PRIORITY_HIGH_ACCURACY, 0).apply {
-//            setGranularity(Granularity.GRANULARITY_PERMISSION_LEVEL)
-//            setWaitForAccurateLocation(true)
-//        }.build()
-//
-//        mFusedLocationClient = LocationServices.getFusedLocationProviderClient(this)
-//        mFusedLocationClient.requestLocationUpdates(
-//            mLocationRequest,
-//            mLocationCallback,
-//            Looper.myLooper()
-//        )
-//    }
-
-    private fun onPermissionsNotGranted() {
-        Toast.makeText(this, getString(R.string.grant_permissions_toast), Toast.LENGTH_SHORT).show()
-    }
-
-    private fun onLocationNotGranted() {
-        Toast.makeText(this, getString(R.string.turn_on_location_toast), Toast.LENGTH_SHORT).show()
-    }
 
     private val requestPermissionLauncher = registerForActivityResult(
         ActivityResultContracts.RequestPermission(),
@@ -147,18 +26,35 @@ class MainActivity : AppCompatActivity() {
         }
     }
 
-    private fun askNotificationPermission() {
+    override fun onCreate(savedInstanceState: Bundle?) {
+        super.onCreate(savedInstanceState)
+        setContentView(R.layout.activity_main)
+
+        requestNotificationPermission()
+
+        // Bottom Navigation
+        val navHostFragment = supportFragmentManager.findFragmentById(R.id.nav_host_fragment) as NavHostFragment
+        val bottomView = findViewById<BottomNavigationView>(R.id.bottom_nav)
+        setupWithNavController(bottomView, navHostFragment.navController)
+    }
+
+    private fun requestNotificationPermission() {
         // This is only necessary for API level >= 33 (TIRAMISU)
         if (Build.VERSION.SDK_INT >= Build.VERSION_CODES.TIRAMISU) {
-            if (ContextCompat.checkSelfPermission(this, Manifest.permission.POST_NOTIFICATIONS) ==
-                PackageManager.PERMISSION_GRANTED
-            ) {
+            if (checkNotificationPermission()) {
                 // FCM SDK (and your app) can post notifications.
-            } else if (shouldShowRequestPermissionRationale(Manifest.permission.POST_NOTIFICATIONS)) {
-            } else {
+            } else if (!shouldShowRequestPermissionRationale(Manifest.permission.POST_NOTIFICATIONS)) {
                 // Directly ask for the permission
                 requestPermissionLauncher.launch(Manifest.permission.POST_NOTIFICATIONS)
             }
         }
     }
+
+    private fun checkNotificationPermission() =
+        if (Build.VERSION.SDK_INT >= Build.VERSION_CODES.TIRAMISU) {
+            ContextCompat.checkSelfPermission(this, Manifest.permission.POST_NOTIFICATIONS) ==
+                    PackageManager.PERMISSION_GRANTED
+        } else {
+            true
+        }
 }
--- app/src/main/java/com/mahmoudhamdyae/weatherforecast/presentation/home/HomeFragment.kt
+++ app/src/main/java/com/mahmoudhamdyae/weatherforecast/presentation/home/HomeFragment.kt
@@ -1,23 +1,38 @@
 package com.mahmoudhamdyae.weatherforecast.presentation.home
 
+import android.Manifest
+import android.annotation.SuppressLint
+import android.content.Context
+import android.content.IntentSender
+import android.content.pm.PackageManager
+import android.location.LocationManager
 import android.os.Bundle
+import android.os.Looper
 import android.view.LayoutInflater
 import android.view.View
 import android.view.ViewGroup
 import android.widget.Toast
+import androidx.core.app.ActivityCompat
 import androidx.databinding.DataBindingUtil
 import androidx.fragment.app.Fragment
 import androidx.fragment.app.viewModels
+import com.google.android.gms.common.api.ResolvableApiException
+import com.google.android.gms.location.*
+import com.google.android.gms.tasks.Task
 import com.mahmoudhamdyae.weatherforecast.R
 import com.mahmoudhamdyae.weatherforecast.databinding.FragmentHomeBinding
 import dagger.hilt.android.AndroidEntryPoint
+
+private const val PERMISSION_Location_ID = 5005
 
 @AndroidEntryPoint
 class HomeFragment : Fragment() {
 
     private lateinit var binding: FragmentHomeBinding
+
     private lateinit var todayAdapter: TodayAdapter
     private lateinit var nextDaysAdapter: NextDaysAdapter
+    private lateinit var mFusedLocationClient: FusedLocationProviderClient
 
     override fun onCreateView(
         inflater: LayoutInflater, container: ViewGroup?,
@@ -29,6 +44,9 @@ class HomeFragment : Fragment() {
 
     override fun onViewCreated(view: View, savedInstanceState: Bundle?) {
         super.onViewCreated(view, savedInstanceState)
+
+        checkLocationPermissions()
+        getLocation()
 
         val viewModel: HomeViewModel by viewModels()
 
@@ -52,5 +70,120 @@ class HomeFragment : Fragment() {
 
 
         viewModel.getWeather(33.23, 31.32)
+
+        mFusedLocationClient = LocationServices.getFusedLocationProviderClient(requireContext())
+    }
+
+    private fun checkLocationPermissions() = ActivityCompat.checkSelfPermission(
+        requireContext(),
+        Manifest.permission.ACCESS_COARSE_LOCATION
+    ) == PackageManager.PERMISSION_GRANTED ||
+            ActivityCompat.checkSelfPermission(
+                requireContext(),
+                Manifest.permission.ACCESS_COARSE_LOCATION
+            ) == PackageManager.PERMISSION_GRANTED
+
+    private fun getLocation() {
+        if (checkLocationPermissions()) {
+            if (isLocationEnabled()) {
+                requestNetworkLocalData()
+            } else {
+                onLocationNotGranted()
+                openLocationDialog(requireContext())
+            }
+        } else {
+            requestLocationPermissions()
+        }
+    }
+
+    private fun openLocationDialog(context: Context) {
+        val locationRequest = LocationRequest.Builder(Priority.PRIORITY_HIGH_ACCURACY, 10000).apply {
+            setGranularity(Granularity.GRANULARITY_PERMISSION_LEVEL)
+            setWaitForAccurateLocation(true)
+        }.build()
+
+        val builder = LocationSettingsRequest.Builder().addLocationRequest(locationRequest)
+        val client: SettingsClient = LocationServices.getSettingsClient(context)
+        val task: Task<LocationSettingsResponse> = client.checkLocationSettings(builder.build())
+
+        task.addOnFailureListener { exception ->
+            if (exception is ResolvableApiException) {
+                // Location settings are not satisfied, but this can be fixed
+                // by showing the user a dialog.
+                try {
+                    // Show the dialog by calling startResolutionForResult(),
+                    // and check the result in onActivityResult().
+                    exception.startResolutionForResult(requireActivity(), 100)
+                } catch (sendEx: IntentSender.SendIntentException) {
+                    // Ignore the error.
+                }
+            }
+        }
+    }
+
+    private fun requestLocationPermissions() {
+        ActivityCompat.requestPermissions(
+            requireActivity(),
+            arrayOf(
+                Manifest.permission.ACCESS_COARSE_LOCATION,
+                Manifest.permission.ACCESS_FINE_LOCATION,
+            ),
+            PERMISSION_Location_ID
+        )
+    }
+
+    private val mLocationCallback = object: LocationCallback() {
+        override fun onLocationResult(locationResult: LocationResult) {
+            super.onLocationResult(locationResult)
+            val mLastLocation = locationResult.lastLocation
+            // todo
+        }
+    }
+
+    override fun onRequestPermissionsResult(
+        requestCode: Int,
+        permissions: Array<out String>,
+        grantResults: IntArray
+    ) {
+        super.onRequestPermissionsResult(requestCode, permissions, grantResults)
+        if (requestCode == PERMISSION_Location_ID) {
+            if (
+                grantResults[0] == PackageManager.PERMISSION_GRANTED ||
+                grantResults[1] == PackageManager.PERMISSION_GRANTED
+            ) {
+                getLocation()
+            } else {
+                onLocationPermissionsNotGranted()
+            }
+        }
+    }
+
+    private fun isLocationEnabled(): Boolean {
+        val locationManager = requireContext().getSystemService(Context.LOCATION_SERVICE) as LocationManager
+        return locationManager.isProviderEnabled(LocationManager.GPS_PROVIDER) ||
+                locationManager.isProviderEnabled(LocationManager.NETWORK_PROVIDER)
+    }
+
+    @SuppressLint("MissingPermission")
+    private fun requestNetworkLocalData() {
+        val mLocationRequest = LocationRequest.Builder(Priority.PRIORITY_HIGH_ACCURACY, 0).apply {
+            setGranularity(Granularity.GRANULARITY_PERMISSION_LEVEL)
+            setWaitForAccurateLocation(true)
+        }.build()
+
+        mFusedLocationClient = LocationServices.getFusedLocationProviderClient(requireContext())
+        mFusedLocationClient.requestLocationUpdates(
+            mLocationRequest,
+            mLocationCallback,
+            Looper.myLooper()
+        )
+    }
+
+    private fun onLocationPermissionsNotGranted() {
+        Toast.makeText(requireContext(), getString(R.string.grant_permissions_toast), Toast.LENGTH_SHORT).show()
+    }
+
+    private fun onLocationNotGranted() {
+        Toast.makeText(requireContext(), getString(R.string.turn_on_location_toast), Toast.LENGTH_SHORT).show()
     }
 }
