504c9608b21edea7bcf4c235708a28fae14eee64
mahmoudhamdyae <mahmoudhamdyae@gmail.com>
2023-11-04 14:11:05 +0200

    Map Activity

--- app/src/main/java/com/mahmoudhamdyae/weatherforecast/presentation/map/MapActivity.kt
+++ app/src/main/java/com/mahmoudhamdyae/weatherforecast/presentation/map/MapActivity.kt
@@ -6,6 +6,12 @@ import android.os.Bundle
 import androidx.activity.viewModels
 import androidx.databinding.DataBindingUtil
 import androidx.lifecycle.ViewModelProvider
+import com.google.android.gms.maps.CameraUpdateFactory
+import com.google.android.gms.maps.GoogleMap
+import com.google.android.gms.maps.OnMapReadyCallback
+import com.google.android.gms.maps.SupportMapFragment
+import com.google.android.gms.maps.model.LatLng
+import com.google.android.gms.maps.model.MarkerOptions
 import com.mahmoudhamdyae.weatherforecast.MainActivity
 import com.mahmoudhamdyae.weatherforecast.R
 import com.mahmoudhamdyae.weatherforecast.databinding.ActivityMapBinding
@@ -13,9 +19,10 @@ import com.mahmoudhamdyae.weatherforecast.domain.model.Location
 import dagger.hilt.android.AndroidEntryPoint
 
 @AndroidEntryPoint
-class MapActivity : AppCompatActivity() {
+class MapActivity : AppCompatActivity(), OnMapReadyCallback {
 
     private lateinit var binding: ActivityMapBinding
+    private lateinit var mMap: GoogleMap
 
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
@@ -24,10 +31,25 @@ class MapActivity : AppCompatActivity() {
         val viewModel: MapViewModel = ViewModelProvider(this)[MapViewModel::class.java]
         binding.lifecycleOwner = this
 
+        val mapFragment = supportFragmentManager.findFragmentById(R.id.map) as SupportMapFragment
+        mapFragment.getMapAsync(this)
+
         binding.addThisLocationButton.setOnClickListener {
             val intent = Intent(this, MainActivity::class.java)
             startActivity(intent)
             finish()
         }
+    }
+
+    override fun onMapReady(googleMap: GoogleMap) {
+        mMap = googleMap
+
+        // Add a marker in Sydney and move the camera
+        val sydney = LatLng(-34.0, 151.0)
+        mMap.addMarker(
+            MarkerOptions()
+            .position(sydney)
+            .title("Marker in Sydney"))
+        mMap.moveCamera(CameraUpdateFactory.newLatLng(sydney))
     }
 }
