54b29c021360ada7c8d86833dde308a8deb402f2
mahmoudhamdyae <mahmoudhamdyae@gmail.com>
2023-11-10 11:18:13 +0200

    Alarm Broadcast

--- app/src/main/java/com/mahmoudhamdyae/weatherforecast/alarm/AlarmSchedulerImpl.kt
+++ app/src/main/java/com/mahmoudhamdyae/weatherforecast/alarm/AlarmSchedulerImpl.kt
@@ -2,13 +2,9 @@ package com.mahmoudhamdyae.weatherforecast.alarm
 
 import android.annotation.SuppressLint
 import android.app.AlarmManager
-import android.app.NotificationManager
 import android.app.PendingIntent
 import android.content.Context
 import android.content.Intent
-import android.util.Log
-import androidx.core.app.NotificationCompat
-import com.mahmoudhamdyae.weatherforecast.R
 import java.time.ZoneId
 
 class AlarmSchedulerImpl(
@@ -19,7 +15,6 @@ class AlarmSchedulerImpl(
 
     @SuppressLint("MissingPermission")
     override fun schedule(alarmItem: AlarmItem) {
-        Log.i("hahahahaha", "schedule: ${alarmItem.message}")
         val intent = Intent(context, AlarmReceiver::class.java).apply {
             putExtra("EXTRA_MESSAGE", alarmItem.message)
         }
@@ -34,20 +29,6 @@ class AlarmSchedulerImpl(
                 PendingIntent.FLAG_UPDATE_CURRENT or PendingIntent.FLAG_IMMUTABLE
             )
         )
-
-
-
-        val channelId = "alarm_id"
-        context?.let { ctx ->
-            val notificationManager =
-                ctx.getSystemService(Context.NOTIFICATION_SERVICE) as NotificationManager
-            val builder = NotificationCompat.Builder(ctx, channelId)
-                .setSmallIcon(R.drawable.ic_launcher_foreground)
-                .setContentTitle("Alarm Demo")
-                .setContentText("Notification sent with message ${alarmItem.message}")
-                .setPriority(NotificationCompat.PRIORITY_HIGH)
-            notificationManager.notify(1, builder.build())
-        }
     }
 
     override fun cancel(alarmItem: AlarmItem) {
